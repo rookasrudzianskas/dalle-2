c4de09960ae609150d0753fb6689abd47dd92ae4
Rokas <rookas.rudzenskas@gmail.com>
2023-03-24 15:42:29 +0200

    Add Azure Functions settings and update GET request URL in suggestion route

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,6 +1,6 @@
 export async function GET(request: Request) {
   const response = await fetch(
-    "....", {cache: "no-store"}
+    "http://localhost:7071/api/getChatGPTSuggestion", {cache: "no-store"}
   );
   const textData = await response.text();
   return new Response(JSON.stringify(textData.trim()), {
