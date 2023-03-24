b73b0836b6ca57d7ab85ec3a0fba42cafd823af7
Rokas <rookas.rudzenskas@gmail.com>
2023-03-24 14:04:35 +0200

    Add Link component to Header for Github repo and creator's personal website."Add Link component to Header for Github repo and creator's personal website."

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = ({}) => {
   return (
@@ -18,6 +19,17 @@ const Header = ({}) => {
           </h1>
           <h2 className="text-xs">Powered by DALL·E 2 & Chat GPT</h2>
         </div>
+      </div>
+      <div className="flex divide-x text-gray-500 items-center text-xs md:text-base">
+        <Link href="https://www.byrookas.com" className="px-2 font-light text-right">
+          Created by Rokas
+        </Link>
+        <Link
+          href="https://byrookas.com/microsoft-youtube-build"
+          className="px-2 font-light"
+        >
+          Github Repo
+        </Link>
       </div>
     </header>
   );
