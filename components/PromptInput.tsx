a47a9d3a11c85fb15e503ce13e03b01ac2182358
Rokas <rookas.rudzenskas@gmail.com>
2023-03-24 19:09:44 +0200

    FIX

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -71,7 +71,7 @@ const PromptInput = ({}) => {
 
   return (
     <div className="m-10">
-      <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+      <form onSubmit={handleSubmit} className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
         <textarea
           placeholder="Enter your prompt here"
           className="flex-1 p-4 outline-none rounded-md"
@@ -89,15 +89,15 @@ const PromptInput = ({}) => {
         </button>
         <button
           className={`p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400`}
-          // onClick={() => submitPrompt(true)}
-          // disabled={isLoading || isValidating}
+          onClick={() => submitPrompt(true)}
+          disabled={isLoading || isValidating}
           type="button"
         >
           Use Suggestion
         </button>
         <button
           className={`p-4 bg-white text-violet-500 border-none transition-colors duration-200 rounded-b-md md:rounded-r-md md:rounded-bl-none font-bold`}
-          // onClick={mutate}
+          onClick={mutate}
           type="button"
         >
           New Suggestion
